5e1db4fdedfe9ff07f2472d4453ce56cf04b90cd
Kirill Kirpa <kirillkirpa@gmail.com>
2019-02-03 20:24:49 +0500

    Bug dix

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+"use strict";
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
+};
