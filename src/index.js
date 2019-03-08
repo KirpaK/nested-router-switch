7d795f766c3dba7f1962be2a103d2479331f5e02
Kirill Kirpa <kirillkirpa@gmail.com>
2019-03-08 23:45:48 +0500

    release-v1.0.0 Обновил экспорт пакета

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import NestedRouterSwitch from "./NestedRouterSwitch";
+import _NestedRouterSwitch from "./NestedRouterSwitch";
+import _NestedRouter from "./NestedRouter";
 
+export const NestedRouterSwitch = _NestedRouterSwitch;
+export const NestedRouter = _NestedRouter;
 export default NestedRouterSwitch;
